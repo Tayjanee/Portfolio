f23be2a8e2b836136fe0b09a768359b91f66def6
Tayjanee <tm.mathis16@gmail.com>
2022-12-21 07:56:39 -0600

    site deploy

---
+++ portfolio/src/components/Testimonials.js
@@ -0,0 +1,45 @@
+import React from "react";
+// import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
+// import { testimonials } from "../data";
+
+export default function Testimonials() {
+  return (
+    <section id="testimonials">
+      {/* <div className="container px-5 py-10 mx-auto text-center">
+        <UsersIcon className="w-10 inline-block mb-4" />
+        <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-12">
+          Resume
+        </h1>
+        <div className="flex flex-wrap m-4">
+          {testimonials.map((testimonial) => (
+            <div className="p-4 md:w-1/2 w-full">
+              <a href ="https://drive.google.com/file/d/12iEcDtROmyteaagUWBbwVN5z-1SHYt1W/view?usp=share_link" attributes-list> </a> 
+              <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
+                <TerminalIcon className="block w-8 text-gray-500 mb-4" />
+                
+                <img
+                    alt="testimonial"
+                    src={testimonial.images}
+                    className="w-100 square-full object-cover object-center"
+                    
+                  />
+
+                <div className="inline-flex items-center">
+                  
+                  <span className="flex-grow flex flex-col pl-4">
+                    <span className="title-font font-medium text-white">
+                      {testimonial.name}
+                    </span>
+                    <span className="text-gray-500 text-sm uppercase">
+                      {testimonial.company}
+                    </span>
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div> */}
+    </section>
+  );
+}
---
+++ portfolio/src/data.js
@@ -0,0 +1,49 @@
+export const projects = [
+  {
+    title: "Parks Pioneer",
+    subtitle: "Full React Website utilizing 2 API Intergrations, Axios, Bootstrap, JavaScript, PostgreSQL and Sequelize  ",
+    description:
+      "This website is dedicated to planning your day at a National Park, with an integrated Google Calendar that allows you to see weather. activities and events at parks all over the U.S. You can also view live-feeds from each park!My role in this project was to intergrate multiple boostrap components, integrate API and modify CSS elements",
+    image: "park.png",
+    link: "https://github.com/Tayjanee/CapstoneProject",
+  },
+  {
+    title: "Random Quote Generator",
+    subtitle: "Utilizes two API integrations, bootstrap and PostgreSQL",
+    description:
+      "single Webpage that utilizes two API integrations, bootstrap and PostgreSQL to randomly generate quotes and photos based on chosen topic. My role in this project was to modify CSS features using boostrap and integrate a quotes API",
+    image: "random.png",
+    link: "https://github.com/Pedrofer98/FRONT-END-Project",
+  },
+  {
+    title: "Gym Planner App",
+    subtitle: "Utilizes React, Redux, PostgreSQL and Bootstrap",
+    description:
+      "Full React blog website dedicated to sharing and planning new workouts. My role in this group project was to creaate our database using PostgreSQL and modify CSS features",
+    image: "Gym.png",
+    link: "https://github.com/klhkassel/full-stack-project",
+  },
+  {
+    title: "Restuarant Page",
+    subtitle: "Updated Restaurant Page utilizing HTML and CSS Grid",
+    description:
+      "Individual Project utilizing HTML and CSS to update webpage of a restaurant",
+    image: "rest.png",
+    link: "https://github.com/Tayjanee/restaurant",
+  },
+];
+
+export const testimonials = [
+
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Sequelize",
+  "Node",
+  "PostgreSQL",
+  "API Integration",
+  "Bootstrap",
+  "Redux"
+];
